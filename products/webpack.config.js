97151892d9860408aa9e0e7c061879310ab19ec3
Uzair Ahmad <uzairahmad.dev@gmail.com>
2022-03-17 11:29:28 +0500

    Refactoring Products

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -11,9 +11,13 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js',
       exposes: {
-        './ProductsIndex': './src/index'
+        './ProductsIndex': './src/bootstrap'
       },
-      shared: ['faker']
+      shared: {
+        faker: {
+          singleton: true
+        }
+      }
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
